ddf269a510b04876e07019eb053de4e591edc4f8
Chance Phiniezy <phiniezyc@gmail.com>
2020-09-19 04:44:46 -0400

    Add GetCommandById

--- Data/IBrainDrainRepo.cs
+++ Data/IBrainDrainRepo.cs
@@ -5,6 +5,8 @@ namespace  BrainDrain.Data
   // Interface is just a list of the methods provided to the consumer of the interface
     public interface BrainDrainRepo
       {
+        // TODO: What is an IEnumerable?
         IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
       }
 }
