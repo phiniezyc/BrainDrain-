6670c02d822280e7703f3e77c3b20c718f042286
Chance Phiniezy <phiniezyc@gmail.com>
2020-09-22 20:37:37 -0400

    Add ActionResult

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using BrainDrain.Models;
 namespace BrainDrain.Controllers
 {
   [Route("api/commands")]
@@ -6,6 +8,9 @@ namespace BrainDrain.Controllers
   // Use ControllerBase instead of Controller to keep it as lean as possible
   public class CommandsController : ControllerBase
   {
+    public ActionResult <IEnumerable<Command>> GetAllCommands()
+    {
 
+    }
   }
 }
