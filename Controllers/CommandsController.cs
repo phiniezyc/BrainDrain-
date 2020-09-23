c3896aa79095a3adf815dadb02969afd70c15790
Chance Phiniezy <phiniezyc@gmail.com>
2020-09-22 20:26:14 -0400

    Add APiController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 namespace BrainDrain.Controllers
 {
+  [ApiController]
+  // Use ControllerBase instead of Controller to keep it as lean as possible
   public class CommandsController : ControllerBase
   {
 
