1c6477cfb28364e94676bba14f49b100a0f741b5
Chance Phiniezy <phiniezyc@gmail.com>
2020-09-22 20:28:22 -0400

    Add api route definitition

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 namespace BrainDrain.Controllers
 {
+  [Route("api/commands")]
   [ApiController]
   // Use ControllerBase instead of Controller to keep it as lean as possible
   public class CommandsController : ControllerBase
