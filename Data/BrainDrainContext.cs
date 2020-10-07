5f0c680de26d3418d335a596deecd6b18f915fac
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-07 03:36:36 -0400

    Add BrainDrainContext Class

--- Data/BrainDrainContext.cs
+++ Data/BrainDrainContext.cs
@@ -3,6 +3,9 @@ namespace BrainDrain.Data
 {
   public class BrainDrainContext : DbContext
   {
+    public BrainDrainContext(DbContextOptions<BrainDrainContext> opt) : base(opt)
+    {
 
+    }
   }
 }
