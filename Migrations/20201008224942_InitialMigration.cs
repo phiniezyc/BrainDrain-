4a574287b4078ea646db514f3f4cafcee49a707c
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-08 18:51:10 -0400

    Create migration file

---
+++ Migrations/20201008224942_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainDrain.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    HowTo = table.Column<string>(nullable: true),
+                    Line = table.Column<string>(nullable: true),
+                    Platform = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
