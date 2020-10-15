2e495b0faf11e279ccdfe1279d2e3b97e76b8ee6
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-15 19:54:05 -0400

    Add constraints to CreateDto

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 namespace BrainDrain.Dtos
 {
   public class CommandCreateDto
   {
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
   }
 }
