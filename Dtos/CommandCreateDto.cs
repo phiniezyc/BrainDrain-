a2f01e3fbb6961007337d478498dbe8b2a0ade98
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-15 19:57:05 -0400

    Add constraint annotation

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -3,6 +3,7 @@ namespace BrainDrain.Dtos
 {
   public class CommandCreateDto
   {
+        // constraints are important because if bad data is passed it will show a 500 server error, when really the client is at fault.  Add the constraints will show a client 400 error if bad data is passed
         [Required]
         [MaxLength(250)]
         public string HowTo { get; set; }
