02c71b650ff71ea318a9f36efe90275cd9b7154e
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-15 20:07:13 -0400

    Fill in create update DTO

---
+++ Dtos/CommandUpdateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace BrainDrain.Dtos
+{
+  public class CommandUpdateDto
+  {
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Line { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+  }
+}
