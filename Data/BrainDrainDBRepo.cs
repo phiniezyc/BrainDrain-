5071e3f3e57f5b4a18a5dad4f4d48f4b0313e36c
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-17 14:12:18 -0400

    Add Delete Command interface

--- Data/BrainDrainDBRepo.cs
+++ Data/BrainDrainDBRepo.cs
@@ -21,6 +21,11 @@ namespace BrainDrain.Data {
       _context.Commands.Add(cmd);
     }
 
+    public void DeleteCommand(Command cmd)
+    {
+      throw new NotImplementedException();
+    }
+
     public IEnumerable<Command> GetAllCommands()
     {
       return _context.Commands.ToList();
