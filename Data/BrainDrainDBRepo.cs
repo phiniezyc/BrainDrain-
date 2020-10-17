66c9e47ecbcf0fafc32290a265ba09cadf4936ba
Chance Phiniezy <phiniezyc@gmail.com>
2020-10-17 14:14:52 -0400

    Add logic to Delete command

--- Data/BrainDrainDBRepo.cs
+++ Data/BrainDrainDBRepo.cs
@@ -23,7 +23,11 @@ namespace BrainDrain.Data {
 
     public void DeleteCommand(Command cmd)
     {
-      throw new NotImplementedException();
+      if (cmd == null)
+      {
+        throw new ArgumentNullException(nameof(cmd));
+      }
+      _context.Commands.Remove(cmd);
     }
 
     public IEnumerable<Command> GetAllCommands()
